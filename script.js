e6b136858ed2afdfb2e99af7ed4cdc366c1bc05e
1mystic <atharvkhare18@gmail.com>
2025-06-21 19:13:58 +0530

    completed_design

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 // Mobile Navigation
-const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const navLinks = document.querySelectorAll('.nav-link');
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
